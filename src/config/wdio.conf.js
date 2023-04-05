8352bc1dfb40ede94a26db25b15e16a3cc21a36b
AntonS <anteydrummer@gmail.com>
2023-04-05 12:59:16 +0300

    moved package and git.ignore outside src

--- src/config/wdio.conf.js
+++ src/config/wdio.conf.js
@@ -191,37 +191,6 @@ exports.config = {
      */
     // onWorkerStart: function (cid, caps, specs, args, execArgv) {
     // },
-    onComplete: function () {
-
-        const reportError = new Error('Could not generate Allure report');
-    
-        const generation = allure(['generate', 'allure-results', '--clean']);
-    
-        return new Promise((resolve, reject) => {
-    
-          const generationTimeout = setTimeout(() => reject(reportError), 5000);
-    
-          generation.on('exit', function (exitCode) {
-    
-            clearTimeout(generationTimeout);
-    
-     
-    
-            if (exitCode !== 0) {
-    
-              return reject(reportError);
-    
-            }
-    
-            console.log('Allure report successfully generated');
-    
-            resolve();
-    
-          });
-    
-        });
-    
-      },
     /**
      * Gets executed just after a worker process has exited.
      * @param  {String} cid      capability id (e.g 0-0)
